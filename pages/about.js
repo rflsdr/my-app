3379e60353afced1244c8a60c403107ff992c87a
rflsdr <sedrakyan.rafael@gmail.com>
2023-05-22 19:50:17 -0400

    dsa
    adsadsadsadsadsa
     dsadsa dsadsa

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const About = () => {
+  return (
+    <div>
+        <Navbar/>
+      <h1>About Us</h1>
+      <p>This is the about us page</p>
+    </div>
+  );
+};
+
+export default About;
